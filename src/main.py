09cd57c2f391225bcbdd2da1ae4c1113ece86ac7
micah price <98mprice@gmail.com>
2018-07-18 17:28:52 +0200

    removed main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-
-parser = ArgumentParser()
-parser.add_argument("-n", "--name", dest="name",
-                    help="name of video without extension")
-
-args = parser.parse_args()
-d = vars(args)
-print d['name']
